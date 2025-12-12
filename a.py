0cf251d1fae103f97a8cd8cae618fcc9f1313817
Harshil <harshil.ghataliya@digiwagon.com>
2025-12-12 11:16:58 +0000

    add login function

--- a.py
+++ a.py
@@ -4,5 +4,14 @@ print("New New change 2")
 print("login added")
 print("new login feature")
 print("new login feature v2")
+
+
 def login():
-    pass
+    user=input("Enter username: ")
+    password=input("Enter password: ")
+    print(f"Logging in {user}...")
+    user=check_credentials(user, password)
+    if user:
+        print("Login successful!")
+    else:
+        print("Login failed. Please check your credentials.")
